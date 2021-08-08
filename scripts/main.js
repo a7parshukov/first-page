02cf084a528b3bbc05c29a2adbc6ea3584a0bcd5
a7parshukov <a7parshukov@gmail.com>
2021-08-08 23:29:34 +0300

    add changing pictrue and save name user in localStorage

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,32 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Немного о сайте';
+// Function for image changer
+let webImage = document.querySelector('img');
+webImage.onclick = function() {
+    let webSrc = webImage.getAttribute('src');
+    if(webSrc === 'img/teacher-5662610_1280.png') {
+        webImage.setAttribute('src', 'img/teacher-5602475_1280.png');
+    } else {
+        webImage.setAttribute('src', 'img/teacher-5662610_1280.png');
+    }
+}
+
+// Personalized welcome message
+let userButton = document.querySelector('button');
+let userHeading = document.querySelector('h2');
+function setUserName() {
+    let userName = prompt('Пожалуйста, введите свое имя...');
+    if(!userName){
+        setUserName();
+    } else {
+        localStorage.setItem('name', userName);
+        userHeading.textContent = 'Привет, ' + userName + ', немного обо мне';
+    }
+}
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storageName = localStorage.getItem('name');
+    userHeading.textContent = 'Привет, ' + storageName + ', немного обо мне';
+}
+userButton.onclick = function() {
+    setUserName();
+}
